72ee1843134ba59667cf046fd0ff0a79772ee437
Lucas Rocha <lucas.diegorr@gmail.com>
2023-11-16 01:08:25 -0300

    Added configuration in the OCI provider

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ terraform {
 
   required_version = ">= 1.6.0, < 2.0.0"
 
+  backend "remote" {
+    organization = "lucasdiegorr"
+    workspaces {
+      name = "oci-cluster"
+    }
+  }
+
   required_providers {
     oci = {
       source  = "oracle/oci"
@@ -11,4 +18,9 @@ terraform {
 }
 
 provider "oci" {
+  region       = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  private_key  = var.private_key
 }
