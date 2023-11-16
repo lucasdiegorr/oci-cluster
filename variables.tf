6dd63d267f8de37a67fd4e6c403f223476c5e7b3
Lucas Rocha <lucas.diegorr@gmail.com>
2023-11-16 00:52:42 -0300

    Added variables

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key" {
+}
+
+variable "ssh_public_key" {
+}
+
+variable "compartment_ocid" {
+}
+
+variable "region" {
+  default = "sa-saopaulo-1"
+}
+
+variable "num_instances" {
+  default = 2
+}
+
+variable "instance_shape" {
+  default = "VM.Standard.E2.1.Micro"
+}
+
+variable "instance_image_ocid" {
+  # See https://docs.oracle.com/en-us/iaas/images/
+  # Canonical-Ubuntu-22.04-2023.09.28-0
+  default = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaa4dcybjf44hvj46xwkroaz4wlk5kshtppxebyimba2y3fh5g6d5ia"
+}
+
+variable "instance_ocpus" { default = 1 }
+
+variable "instance_shape_config_memory_in_gbs" { default = 1 }
