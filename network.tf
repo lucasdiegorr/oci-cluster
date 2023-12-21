a4b6ecc0e2bdd013172660bb78ea7538e533a9d2
Lucas Diego <lucas.diegorr@gmail.com>
2023-12-21 16:46:07 +0000

    Created ingress police to access

--- network.tf
+++ network.tf
@@ -73,4 +73,15 @@ resource "oci_core_security_list" "oci_homelab_security_list" {
       min = "80"
     }
   }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "6443"
+      min = "6443"
+    }
+  }
+
 }
